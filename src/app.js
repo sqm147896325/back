cedd7281f3b829f1579ebea7fe36a0256f9d7ed2
sqm147896325 <43403508+sqm147896325@users.noreply.github.com>
2020-08-20 09:10:54 +0800

    提交测试

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
-import React,{Component} from 'react'
-// React,{Component} 其中React是接收导出的变量。
-// Component为React中的属性，将它单独存入变量
-import ReactDom from 'react-dom'
+import React from 'react'
+import {Button} from 'antd'
+// {}内为该模块的一个属性
+// 开发完成一般按需打包
+import 'antd/dist/antd.css'
+// 导入样式文件
 
-export default class extends React.Component{
-    render(
-        return (<div>
-                123
-            </div>)
+export default class extends React.Component {
+  render () {
+    return (
+        <div>
+        <Button type="primary">123</Button>
+        </div>
     )
+  }
 }
