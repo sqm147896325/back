a59e4f58f6ab35c94367ee01e86fb292374e0c98
sqm147896325 <43403508+sqm147896325@users.noreply.github.com>
2020-08-20 08:22:40 +0800

    更改

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+// React,{Component} 其中React是接收导出的变量。
+// Component为React中的属性，将它单独存入变量
+import ReactDom from 'react-dom'
+
+export default class extends React.Component{
+    render(
+        return (<div>
+                123
+            </div>)
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './app'
+// 引入自定义模块需要加./
+
+ReactDom.render(<App />,document.getElementById('root'))
