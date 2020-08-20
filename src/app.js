9f3fe25825b8439e6142563991c44f33f9b57ba0
sqm147896325 <1435585893@qq.com>
2020-08-20 11:20:55 +0800

    demo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import {Button} from 'antd'
+import { Button, Switch } from 'antd'
 // {}内为该模块的一个属性
 // 开发完成一般按需打包
-import 'antd/dist/antd.css'
-// 导入样式文件
+// import 'antd/dist/antd.css'
+// 导入样式文件，按需打包配置后不需要导入
+import { BrowserRouter, Route ,Link } from 'react-router-dom'
+import Login from './pages/login/login.jsx'
+import Admin from './pages/admin/admin.jsx'
 
 export default class extends React.Component {
   render () {
     return (
-        <div>
-        <Button type="primary">123</Button>
-        </div>
+      <div>
+      <BrowserRouter>
+      <Switch>
+      <Link to="/login">123</Link>
+      <Link to="/admin">123</Link>
+      <Route path='/login' component={Login}></Route>
+      {/* 注意path的格式一定要是 /路径 这样 */}
+      <Route path='/admin' component={Admin}></Route>
+      </Switch>
+      </BrowserRouter>
+      </div>
     )
   }
 }
